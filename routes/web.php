596613d45a68a510bd747a2fa64c7a09e8591be2
perdidev <perdidev@gmail.com>
2024-08-07 11:07:37 +0800

    fix: route auth/* to api/auth/*

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,1 @@
 <?php
-
-use App\Http\Controllers\Auth\GoogleController;
-
-Route::get('auth/google/redirect', [GoogleController::class, 'redirectToGoogle']);
-
-Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
-
-
